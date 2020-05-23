94842154e18c1a8a8b87ede7da487d67bd896b6f
Sutter Grune <suttergrune@gmail.com>
2020-05-23 08:07:20 -0400

    clean up

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,8 +2,5 @@
 drop table counties;
 drop table states;
 
---create table states(id serial primary key, state varchar not null, lease_count integer);
 create table states(id integer primary key, state varchar, lease_count integer);
-
---create table counties(id serial primary key, name varchar not null, state varchar not null, lease_count integer);
 create table counties(id integer primary key, name varchar, state varchar, lease_count integer);
--- src/test/java/app/util/AppPropertiesTest.java
+++ src/test/java/app/util/AppPropertiesTest.java
@@ -1,8 +1,7 @@
 package app.util;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertTrue;
 import static app.util.AppProperties.PROPERTIES;
 
 public class AppPropertiesTest {
@@ -10,7 +9,7 @@ public class AppPropertiesTest {
     @Test
     public void GetPropertiesTest() {
         String user = PROPERTIES.getProperty("DATABASE_USER");
-        assertEquals("user1", user);
+        assertTrue(user != null && !user.isEmpty());
     }
     
 }
